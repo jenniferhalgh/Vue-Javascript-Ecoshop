7775ea189a45fc633f20e92a3e8228eed1827601
AnashaSarker2001 <102026572+AnashaSarker2001@users.noreply.github.com>
2022-09-15 16:12:51 +0200

    The order controller is fixed

--- server/controllers/orders.js
+++ server/controllers/orders.js
@@ -1,25 +1,48 @@
 var express = require("express");
 var router = express.Router();
 const Item = require("../models/item");
+const Customer = require("../models/customer");
 var Order = require("../models/order");
 const Store = require("../models/store");
 
 
-//store customer's orders
-router.post("/orders", function(req, res){
-    var orders = new Order(req.body);
-    orders.save(function(err) {
-        if (err) { return res.status(500).send(err);}
-        res.status(201).json(orders);
-        console.log(orders);
+//create customer's order
+router.post("/customers/:id/orders", function(req, res){
+    var id = req.params.id;
+    Customer.findById(id, function(err, customer){
+        if (err) {return res.status(500).send(err);}
+        if (customer == null) {
+            return res.status(404).json({"message": "Customer not found"});
+            
+        }
+        var order = new Order(req.body);
+        order.save(
+            function(err) { 
+            if (err) {return res.status(500)}
+            console.log(order);}
+        );
+        customer.orders.push(order);
+        customer.save();
+        return res.status(201).json(customer.orders);
     });
 });
-//get customer's orders
-router.get('/orders', function(req, res){
-    Order.find(function(err, orders){
-        if(err){return res.status(500).send(err);}
-        res.status(200).json({"orders": orders});
-    });
+//get customer's order
+router.get('/customers/:customers_id/orders', function(req, res){
+    var id = req.params.customers_id;
+    Customer.findById(id).populate("orders").exec(function(err, customer){
+    if (err) {return res.status(500).send(err);}
+    if (customer == null) {
+        return res.status(404).json({"message": "Customer not found"});
+    }
+    res.status(200).send(customer.orders);
+    })
 });
 
+//gets all orders
+router.get('/orders', function(req, res){
+    Order.find(function(err, order){
+        if(err){return res.status(500).send(err);}
+        res.status(200).json({"orders": order});
+    });
+});
 module.exports = router;
