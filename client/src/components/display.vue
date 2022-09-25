9d8905192c692e32b8f69c6032f3e17f8f0b762c
AnashaSarker2001 <102026572+AnashaSarker2001@users.noreply.github.com>
2022-09-26 00:20:52 +0200

    Profile page created

---
+++ client/src/components/display.vue
@@ -0,0 +1,46 @@
+<template>
+    <div class="text">
+          <div style="display: flex">
+            <p class="attribute">First Name:</p>
+            <p>{{ customer.name.firstname }}</p>
+          </div>
+          <div style="display: flex">
+            <p class="attribute">Email:</p>
+            <p>{{ customer.email }}</p>
+          </div>
+          </div>
+</template>
+
+<script>
+// @ is an alias to /src
+
+export default {
+  name: 'display',
+  mounted() {
+    const jwttoken = {
+      token: sessionStorage.getItem('token')
+    }
+    fetch('http://localhost:3000/customer', {
+      method: 'GET',
+      mode: 'cors',
+      headers: {
+        'Content-Type': 'application/json',
+        Host: '',
+        token: jwttoken.token
+      }
+    }).then((response) => {
+      return response.json()
+    }).then((responseData) => {
+      this.customer = responseData
+    }).catch(function (err) {
+      console.log(err)
+    })
+  },
+  data() {
+    return {
+      customer: {}
+    }
+  }
+
+}
+</script>
