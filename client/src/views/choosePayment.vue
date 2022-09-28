a4257cc6889b0c451d1eb6a7f2baa915d110b4b6
jenniferhalgh <halgh@student.chalmers.se>
2022-09-28 16:50:21 +0200

    Changed the buttons to redired to different pages

--- client/src/views/choosePayment.vue
+++ client/src/views/choosePayment.vue
@@ -24,7 +24,7 @@
 <!-- ALL CARDS -->
         <div v-for="payment in paymentInfos" v-bind:key="payment._id">
     <div class="card ml-5 mb-5" style="width: 18rem;">
-      <div class="card-header">Card</div>
+      <div class="card-header">Card <button class="button-update" onclick="document.getElementById('id2').style.display='block'" v-on:click="paymentId=payment._id" style="width: auto">Edit</button></div>
       <ul class="list-group payment list-group-flush">
         <div>
           <li class="list-group-item">Name on card: {{payment.cardInfo.nameOnCard}}</li>
@@ -58,6 +58,41 @@
         <button type="submit" class="btn btn-primary" v-on:click="addNewPayment=false, addPayment()">Add</button>
     </form>
   </div>
+
+  <div id="id2" class="modal">
+      <form class="modal-contents" @submit.prevent="edit()">
+        <div class="container">
+          <div class="form-update">
+            <span
+              onclick="document.getElementById('id2').style.display='none'"
+              class="close"
+              title="Close Modal"
+              >&times;</span
+            >
+            <input
+              type="text"
+              class="form-control"
+              v-model="nameOnCard"
+              placeholder="nameOnCard"
+            />
+            <input
+              type="text"
+              class="form-control"
+              v-model="cardNum"
+              placeholder="Card number"
+            />
+            <input
+              type="number"
+              class="form-control"
+              v-model.lazy="cvv"
+              placeholder="cvv"
+            />
+            <button class="btn btn-primary btn-block">Update</button>
+          </div>
+        </div>
+      </form>
+    </div>
+
   </div>
 
 </template>
@@ -82,13 +117,7 @@ export default {
         token: jwttoken.token
       }
     }).then((response) => {
-      if (response.status === 401) {
-        this.$bvModal.msgBoxOk('Unauthorized. Please log in again')
-        sessionStorage.setItem('token', null)
-        this.$router.push('/signIn')
-      } else {
-        return response.json()
-      }
+      return response.json()
     }).then((responseData) => {
       this.customer = responseData
       const vm = this
@@ -110,40 +139,11 @@ export default {
       nameOnCard: '',
       cardNum: '',
       cvv: '',
-      error: ''
+      error: '',
+      paymentId: ''
     }
   },
   methods: {
-    removeFromCart(item) {
-      const jwttoken = {
-        token: sessionStorage.getItem('token')
-      }
-      fetch('http://localhost:3000/customer', {
-        method: 'GET',
-        mode: 'cors',
-        headers: {
-          'Content-Type': 'application/json',
-          Host: '',
-          token: jwttoken.token
-        }
-      }).then((response) => {
-        return response.json()
-      }).then((responseData) => {
-        this.customer = responseData._id
-        Api.delete(`/customers/${this.customer}/shoppingCart/${item}`).then((res) => {
-          this.$bvModal.msgBoxOk('Removed from cart!')
-        },
-        (err) => {
-          console.log(err.response)
-          this.boxOne = ''
-          this.error = err.response.data.error
-          this.$bvModal.msgBoxOk(this.error)
-        }
-        )
-      }).catch(function (err) {
-        console.log(err)
-      })
-    },
     chosenOption(payment) {
       this.hasChosen = true
       Api.get(`/customers/${this.customer._id}/paymentInfos/${payment._id}`).then((res) => {
@@ -160,41 +160,55 @@ export default {
       )
     },
     addPayment() {
-      const jwttoken = {
-        token: sessionStorage.getItem('token')
+      const newCard = {
+        cardInfo: {
+          nameOnCard: this.nameOnCard,
+          cardNum: this.cardNum,
+          cvv: this.cvv
+        },
+        customer: this.customer._id
       }
-      fetch('http://localhost:3000/customer', {
-        method: 'GET',
-        mode: 'cors',
-        headers: {
-          'Content-Type': 'application/json',
-          Host: '',
-          token: jwttoken.token
-        }
-      }).then((response) => {
-        return response.json()
-      }).then((responseData) => {
-        this.customer = responseData
-        const newCard = {
-          cardInfo: {
-            nameOnCard: this.nameOnCard,
-            cardNum: this.cardNum,
-            cvv: this.cvv
-          },
-          customer: this.customer._id
-        }
-        Api.post(`/customers/${this.customer._id}/paymentInfos`, newCard).then(response => {
-          console.log(response.data)
-          this.paymentInfos = []
-          this.paymentInfos.push(newCard)
-          this.hasChosen = true
-          this.$bvModal.msgBoxOk('New card added')
-        })
+      Api.post(`/customers/${this.customer._id}/paymentInfos`, newCard).then(response => {
+        console.log(response.data)
+        this.paymentInfos = []
+        this.paymentInfos.push(newCard)
+        this.hasChosen = true
+        this.$bvModal.msgBoxOk('New card added')
       }).catch(error => {
         this.error = error.response.data.error
         console.error(error)
         console.log(this.error)
         this.$bvModal.msgBoxOk(this.error)
+      })
+    },
+    edit() {
+      const updatedCard = {
+        cardInfo: {
+          nameOnCard: this.nameOnCard,
+          cardNum: this.cardNum,
+          cvv: this.cvv
+        },
+        customer: this.customer._id
+      }
+      console.log(this.paymentId)
+      console.log(updatedCard)
+      console.log(this.customer._id)
+      Api.put(`/customers/${this.customer._id}/paymentInfos/${this.paymentId}`, updatedCard).then((res) => {
+        console.log(res.data)
+      },
+      (err) => {
+        console.log(err.response)
+        this.boxOne = ''
+        this.error = err.response.data.error
+        this.$bvModal.msgBoxOk(this.error)
+      }
+      )
+      Api.get(`/customers/${this.customer._id}/paymentInfos`).then(response => {
+        console.log(response.data)
+        this.paymentInfos = response.data
+        location.reload()
+      }).catch(error => {
+        console.error(error)
       })
     }
   }
